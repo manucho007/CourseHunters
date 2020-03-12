877bae8f3342ad2cc72e92a5c4a90904024ccc34
Manuel Rodriguez <manrodri92@gmail.com>
2020-03-12 16:16:35 +0300

    JSON and template edited to formly format

--- NgReactiveForms/src/app/app.component.ts
+++ NgReactiveForms/src/app/app.component.ts
@@ -1,59 +1,128 @@
 import { Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>Angular Reactive forms</h1>
-    <app-dynamic-form [formDataObj]="person"></app-dynamic-form>
+    <app-dynamic-form
+      [data]="person"
+      [formDataObj]="personFields"
+    ></app-dynamic-form>
   `,
   styles: []
 })
 export class AppComponent {
   person = {
-    firstName: {
-      value: 'Manuel',
-      label: 'FirstName',
-      type: 'text',
+    firstName: 'Manuel',
+    age: 27,
+    gender: 'M'
+  };
+  // Optimized for FORMLY
+  personFields = [
+    {
+      key: 'firstName',
+      type: 'input',
+      templateOptions: {
+        label: 'First Name'
+      },
       validators: {
-        required: true
+        validation: Validators.required
+      },
+      validation: {
+        messages: {
+          required: 'You need to provide a value'
+        }
       }
     },
-    lastName: {
-      value: 'Rodriguez',
-      label: 'LastName',
-      type: 'text'
-    },
-    age: {
-      value: 27,
-      label: 'Age',
-      type: 'number',
-      validators: { min: 18 }
-    },
-    city: {
-      value: 'NY',
-      label: 'City',
-      type: 'select',
-      options: [
-        { label: '(Choose one)', value: '' },
-        { label: 'New York', value: 'NY' },
-        { label: 'Los Angeles', value: 'LA' }
-      ]
-    },
-    gender: {
-      value: 'M',
-      type: 'radio',
-      label: 'Gender',
-      options: [
-        {
-          label: 'Male',
-          value: 'M'
-        },
-        {
-          label: 'Female',
-          value: 'F'
+    {
+      key: 'lastName',
+      type: 'input',
+      templateOptions: {
+        label: 'Last Name'
+      },
+      validators: {
+        validation: Validators.required
+      },
+      validation: {
+        messages: {
+          required: 'You need to provide a value'
         }
-      ]
+      }
+    },
+    {
+      key: 'age',
+      type: 'input',
+      templateOptions: {
+        label: 'Age',
+        type: 'number'
+      },
+      validators: {
+        validation: Validators.min(18)
+      },
+      validation: {
+        messages: {
+          min: 'Needs to be at least 18'
+        }
+      }
+    },
+    {
+      key: 'gender',
+      type: 'radio',
+      templateOptions: {
+        label: 'gender',
+        options: [
+          { value: 'Male', key: 'M' },
+          { value: 'Female', key: 'F' }
+        ]
+      }
     }
-  };
+  ] as FormlyFieldConfig;
+
+  // Basic Data structure
+  // {
+  //   value: 'Manuel',
+  //   label: 'FirstName',
+  //   type: 'text',
+  //   validators: {
+  //     required: true
+  //   }
+  // },
+  // lastName: {
+  //   value: 'Rodriguez',
+  //   label: 'LastName',
+  //   type: 'text'
+  // },
+  // age: {
+  //   value: 27,
+  //   label: 'Age',
+  //   type: 'number',
+  //   validators: { min: 18 }
+  // },
+  // city: {
+  //   value: 'NY',
+  //   label: 'City',
+  //   type: 'select',
+  //   options: [
+  //     { label: '(Choose one)', value: '' },
+  //     { label: 'New York', value: 'NY' },
+  //     { label: 'Los Angeles', value: 'LA' }
+  //   ]
+  // },
+  // gender: {
+  //   value: 'M',
+  //   type: 'radio',
+  //   label: 'Gender',
+  //   options: [
+  //     {
+  //       label: 'Male',
+  //       value: 'M'
+  //     },
+  //     {
+  //       label: 'Female',
+  //       value: 'F'
+  //     }
+  //   ]
+  // }
 }
--- NgReactiveForms/src/app/dynamic-form.component.ts
+++ NgReactiveForms/src/app/dynamic-form.component.ts
@@ -1,5 +1,5 @@
 import { Validators } from '@angular/forms';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -13,43 +13,46 @@ import { FormGroup, FormControl } from '@angular/forms';
     `
   ]
 })
-export class DynamicFormComponent implements OnInit {
-  form: FormGroup;
+export class DynamicFormComponent {
+  form: FormGroup = new FormGroup({});
+  @Input() data;
   @Input() formDataObj;
-  personProps = [];
-  ngOnInit() {
-    const formDataObj = {};
-    for (const prop of Object.keys(this.formDataObj)) {
-      formDataObj[prop] = new FormControl(
-        this.formDataObj[prop].value,
-        this.mapValidator(this.formDataObj[prop].validators)
-      );
-      /*
-        {
-            key: firstName,
-            label: 'FirstName',
-            type: 'text',
-            options:[]
-        }
-      */
-      this.personProps.push({
-        key: prop,
-        label: this.formDataObj[prop].label,
-        type: this.formDataObj[prop].type,
-        options: this.formDataObj[prop].options
-      });
-    }
-    this.form = new FormGroup(formDataObj);
-  }
-  mapValidator(validators) {
-    if (validators) {
-      return Object.keys(validators).map(validationType => {
-        if (validationType === 'required') {
-          return Validators.required;
-        } else if (validationType === 'min') {
-          return Validators.min(validators[validationType]);
-        }
-      });
-    }
-  }
+  submittedData;
+  // Was handled manually before Formly
+  // personProps = [];
+  // ngOnInit() {
+  //   const formDataObj = {};
+  //   for (const prop of Object.keys(this.formDataObj)) {
+  //     formDataObj[prop] = new FormControl(
+  //       this.formDataObj[prop].value,
+  //       this.mapValidator(this.formDataObj[prop].validators)
+  //     );
+  //     /*
+  //       {
+  //           key: firstName,
+  //           label: 'FirstName',
+  //           type: 'text',
+  //           options:[]
+  //       }
+  //     */
+  //     this.personProps.push({
+  //       key: prop,
+  //       label: this.formDataObj[prop].label,
+  //       type: this.formDataObj[prop].type,
+  //       options: this.formDataObj[prop].options
+  //     });
+  //   }
+  //   this.form = new FormGroup(formDataObj);
+  // }
+  // mapValidator(validators) {
+  //   if (validators) {
+  //     return Object.keys(validators).map(validationType => {
+  //       if (validationType === 'required') {
+  //         return Validators.required;
+  //       } else if (validationType === 'min') {
+  //         return Validators.min(validators[validationType]);
+  //       }
+  //     });
+  //   }
+  // }
 }
