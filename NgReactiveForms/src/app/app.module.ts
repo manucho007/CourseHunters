00b53d847cdca657167e229f1dcd01f198840c28
Manuel Rodriguez <manrodri92@gmail.com>
2020-03-12 16:15:36 +0300

    Added: Formly

--- NgReactiveForms/src/app/app.module.ts
+++ NgReactiveForms/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent, DynamicFormComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
