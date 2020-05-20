9eb4763909a9d1c421eb960e55900dda373fb712
Manuel Rodriguez <manrodri92@gmail.com>
2020-05-20 22:17:17 +0300

    Added tests for async functions with mocha

---
+++ Guide to testing JS and Node Apps/test/parse-stringify.spec.js
@@ -0,0 +1,21 @@
+const { parse, stringify } = require('../parse-stringify');
+const assert = require('assert');
+describe('The stringify function', () => {
+  it('should stringify an object into a valid query string', () => {
+    const actual = stringify({ by: 'kati-frantz' });
+    const expected = 'by=kati-frantz';
+    assert.equal(actual, expected);
+    // expect(actual).toBe(expected);
+  });
+});
+
+describe('The parse function', () => {
+  it('should parse a query string into an object', () => {
+    const actual = parse('?by=kati-frantz');
+    const expected = { by: 'kati-frantz' };
+    // Deep equal is used to compare properties between 2 objects
+    assert.deepEqual(actual, expected);
+
+    // expect(actual).toEqual(expected);
+  });
+});
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Angular Service Injection with Dependency Injector
 - Dynamic Reactive forms in Angular
 - Reactive Angular Course
+- The complete guide to testing javascript & node applications
