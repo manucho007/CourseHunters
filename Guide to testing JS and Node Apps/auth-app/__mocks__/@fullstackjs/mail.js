b56dffaea8345f36aa36c70666f9333064cd958f
Manuel Rodriguez <manrodri92@gmail.com>
2020-06-01 00:35:03 +0300

    Added mockup mail service

---
+++ Guide to testing JS and Node Apps/auth-app/__mocks__/@fullstackjs/mail.js
@@ -0,0 +1,18 @@
+module.exports = class Mail {
+    to() {
+        return this
+    }
+    subject() {
+        return this
+    }
+    return() {
+        return this
+    }
+
+    data() {
+        return this
+    }
+    send() {
+        return this
+    }
+}
